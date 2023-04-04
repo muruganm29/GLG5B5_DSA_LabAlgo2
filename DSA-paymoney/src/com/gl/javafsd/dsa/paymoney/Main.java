31b633b0c541260a14acaf838fad991e7d2599f9
murugan <127122582+muruganm29@users.noreply.github.com>
2023-04-04 22:28:12 +0530

    GLG5B5_DSA_LabAlgo2

---
+++ DSA-paymoney/src/com/gl/javafsd/dsa/paymoney/Main.java
@@ -0,0 +1,12 @@
+
+package com.gl.javafsd.dsa.paymoney;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		PayMoneyTransactionManager manager = new PayMoneyTransactionManager();
+		
+		manager.process();
+	}
+}
